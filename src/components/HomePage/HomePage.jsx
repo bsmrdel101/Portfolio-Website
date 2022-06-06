0bcf54f38fb97d844e907c371564f1133aae2aa8
Bennett Smrdel <smrdelb@gmail.com>
2022-06-06 08:53:16 -0500

    added lottie animations into the project

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import lottie from 'lottie-web';
 
 import Header from '../Header/Header';
 import './HomePage.css';
 
 function HomePage() {
+  useEffect (() => {
+    lottie.loadAnimation({
+      container: document.getElementById('rocket-anim'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets3.lottiefiles.com/packages/lf20_l3qxn9jy.json' // the path to the animation json
+    });
+  }, []);
+
   return (
     <div className="container">
       <Header />
         <center>
           <img className='profile-pic' src='profile_pic.jpeg' />
+          <div id='rocket-anim'></div>
         </center>
     </div>
   );
