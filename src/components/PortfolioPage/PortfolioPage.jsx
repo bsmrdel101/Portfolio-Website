aeb1a95a1b298548e793f77b6054a0879b9de0fd
Bennett Smrdel <smrdelb@gmail.com>
2022-06-06 15:03:12 -0500

    added all the information for my web applications in the portfolio

--- src/components/PortfolioPage/PortfolioPage.jsx
+++ src/components/PortfolioPage/PortfolioPage.jsx
@@ -57,51 +57,86 @@ function PortfolioPage() {
                 <Grid container spacing={2}>
                     {/* Web Applications */}
                     <Grid item xs={12}>
-                        <h1>Web Applications</h1>
+                        <h1 className='portfolio-subtitle'>Web Applications</h1>
                     </Grid>
-                    <Grid item sm={12} lg={6}>
-                        <div className='small-container reveal'>
-                            <h1>Chore Slayer</h1>
+                    <Grid item sm={12} lg={10}>
+                        <div className='small-container'>
+                            <h2 className='card-name'>Chore Slayer</h2>
+                            <div className='card-content'>
+                                <p className='card-description'>Chore Slayer is a task management app, with an AI driven card game built on top of it. The app helps incentivise you to complete your tasks, by rewarding you with “coins” for every 3 tasks completed. You can spend these coins in the store to buy cards, and manage your deck which will be used in the game.  On the game view, you play against an AI in a turn based strategy game(Similar to Hearthstone or Magic the Gathering). The AI gets harder over time as you add new cards to your deck, because it will also add more powerful cards to its deck. There’s also a statistics page to help track your stats from the games you play, and a help page that goes over how to use the app.</p>
+                                <img className='card-img' src='https://github.com/bsmrdel101/Chore-Slayer/raw/master/public/pic_2.png' />
+                            </div>
+                            <div className='app-link-container'>
+                                <p className='app-link' onClick={() => window.open("https://github.com/bsmrdel101/Chore-Slayer")}>
+                                    Github Repository
+                                </p>
+                                <p className='app-link' onClick={() => window.open("https://chore-slayer.herokuapp.com/#/home")}>
+                                    Deployed App
+                                </p>
+                            </div>
                         </div> 
                     </Grid>
                     <Grid item xs={12}></Grid>
-                    <Grid item xs={6}></Grid>
-                    <Grid item sm={12} lg={6}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item sm={12} lg={10}>
                         <div className='small-container reveal'>
-                            <h1>Internship on Demand</h1>
-                        
+                            <h2 className='card-name'>Internship on Demand</h2>
+                            <div className='card-content'>
+                                <p className='card-description'>IOD (Internship on Demand) is an app that grants students an easier and more simplified experience with applying for internships. Students enrolled at the courses at IOD will be able to register on this app, create a profile, and search for an internship to apply for. They will also be able to showcase their work on their own portfolio page.</p>
+                                <img className='card-img' src='https://github.com/bsmrdel101/IOD-1/raw/master/public/iodGif.gif' />
+                            </div>
+                            <div className='app-link-container'>
+                                <p className='app-link' onClick={() => window.open("https://github.com/bsmrdel101/IOD-1")}>
+                                    Github Repository
+                                </p>
+                                <p className='app-link' onClick={() => window.open("https://internship-on-demand-dev.herokuapp.com/#/user")}>
+                                    Deployed App
+                                </p>
+                            </div>
                         </div> 
                     </Grid>
-                    <Grid item xs={6}></Grid>
+                    <Grid item xs={4}></Grid>
                     <Grid item xs={12}></Grid>
-                    <Grid item sm={12} lg={6}>
+                    <Grid item sm={12} lg={10}>
                         <div className='small-container reveal'>
-                            <h1>SH Compliance and Consulting</h1>
+                            <h2 className='card-name'>SH Compliance and Consulting</h2>
+                            <div className='card-content'>
+                                <p className='card-description'>This consulting company commissioned me to build them a website to show off their business to clients. It displays their services, contact, about sections, and a form to schedule a consultation.</p>
+                                <img className='card-img' src='https://github.com/bsmrdel101/sh-service-consulting/blob/master/public/service_consulting_pic_1.png?raw=true' />
+                            </div>
+                            <div className='app-link-container'>
+                                <p className='app-link' onClick={() => window.open("https://github.com/bsmrdel101/sh-service-consulting")}>
+                                    Github Repository
+                                </p>
+                                <p className='app-link' onClick={() => window.open("http://sh-compliance-and-consulting.com/")}>
+                                    Deployed Website
+                                </p>
+                            </div>
                         </div> 
                     </Grid>
                     <Grid item xs={12}></Grid>
 
                     {/* Game Development */}
                     <Grid item xs={12}>
-                        <h1>Game Development</h1>
+                        <h1 className='portfolio-subtitle'>Game Development</h1>
                     </Grid>
-                    <Grid item sm={12} lg={6}>
+                    <Grid item sm={12} lg={8}>
                         <div className='small-container reveal'>
-                            <h1>Time Swap</h1>
+                            <strong><h2 className='card-name'>Time Swap</h2></strong>
                         </div> 
                     </Grid>
                     <Grid item xs={12}></Grid>
-                    <Grid item xs={6}></Grid>
-                    <Grid item sm={12} lg={6}>
+                    <Grid item xs={4}></Grid>
+                    <Grid item sm={12} lg={8}>
                         <div className='small-container reveal'>
-                            <h1>Polarity Doors</h1>
+                            <strong><h2 className='card-name'>Polarity Doors</h2></strong>
                         </div> 
                     </Grid>
-                    <Grid item xs={6}></Grid>
+                    <Grid item xs={4}></Grid>
                     <Grid item xs={12}></Grid>
-                    <Grid item sm={12} lg={6}>
+                    <Grid item sm={12} lg={8}>
                         <div className='small-container reveal last-container'>
-                            <h1>Watch Your Step</h1>
+                            <strong><h2 className='card-name'>Watch Your Step</h2></strong>
                         </div> 
                     </Grid>
                 </Grid>
