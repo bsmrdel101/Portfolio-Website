67a0e8f98995b2517237da814e089fd6c04a5a9f
Bennett Smrdel <smrdelb@gmail.com>
2022-06-05 19:55:11 -0500

    added a header with a name and subtitle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './Header.css';
 
 function Header() {
     return (
         <header className='header'>
-
+            <h1>Bennett Smrdel</h1>
+            <h3>Full Stack Software Engineer</h3>
         </header>
     );
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import Header from '../Header/Header';
 import './HomePage.css';
 
 function HomePage() {
   return (
     <div className="container">
-      
+      <Header />
     </div>
   );
 }
